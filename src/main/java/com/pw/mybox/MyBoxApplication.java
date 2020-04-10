3ca60e1e2b3cf9eac85f032bff93d6ecffb5d287
pishuiwei <823598326@qq.com>
2020-04-10 22:42:53 +0800

    创建项目

---
+++ src/main/java/com/pw/mybox/MyBoxApplication.java
@@ -0,0 +1,13 @@
+package com.pw.mybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBoxApplication.class, args);
+    }
+
+}
